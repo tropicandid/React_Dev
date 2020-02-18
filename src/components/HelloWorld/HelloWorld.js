583fc2868f36488d33521c5b9a2a5c4c1f451617
Deanna Steers <steers@teamsubjectmatter.com>
2020-02-18 13:47:08 -0500

    starter template for local react dev environment

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,8 @@
+ import React from "react";
+ import style from "./HelloWorld.css";
+
+ const HelloWorld = () => {
+   return (<div className={style.Hello}>Hello to the World</div>)
+ }
+
+ module.exports = HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/HelloWorld/HelloWorld"
+
+ReactDOM.render(<Hello />, document.getElementById("main"));
